22af3f76aea82cedb09fbd27a745b2281d76a647
Jack O'Connor <oconnor663@gmail.com>
2015-05-17 10:26:45 -0400

    echo supports -n with docopt

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,6 +1,27 @@
-use std::env;
+extern crate rustc_serialize;
+extern crate docopt;
+
+static USAGE: &'static str = "
+Usage:
+    echo [-n] <args>...
+
+Options:
+  -n    no newline at the end
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_args: Vec<String>,
+    flag_n: bool,
+}
 
 fn main() {
-    let v: Vec<String> = env::args().skip(1).collect();
-    println!("{}", v.connect(" "));
+    let args: Args = docopt::Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    if args.flag_n {
+        print!("{}", args.arg_args.connect(" "));
+    } else {
+        println!("{}", args.arg_args.connect(" "));
+    }
 }
